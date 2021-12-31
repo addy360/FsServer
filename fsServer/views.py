8d635e7e262349ca5b1dc4be2499e29e1cbe0cc6
addy360 <abbyboy360@gmail.com>
2021-12-31 16:00:16 +0300

    directories view

--- fsServer/views.py
+++ fsServer/views.py
@@ -1,12 +1,23 @@
-from django.http.response import HttpResponseBadRequest
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from utils.index import MyFs
+
 @api_view(['GET'])
 def index(request):
+    print(type(request))
     context = {
         "author":"addy360",
         "description":"An application api server to view files from a server or any personal computer through any browser"
     }
+    return Response(context)
+
+@api_view(['get'])
+def directories(request):
+    fs = MyFs()
+
+    context = {
+        "data":fs.list_dirs()
+    }
+
     return Response(context)
---
+++ utils/index.py
@@ -0,0 +1,53 @@
+import pathlib
+
+class MyFs():
+    def __init__(self):
+        self.path = pathlib.Path.home()
+
+    def set_path(self,path):
+        self.path = pathlib.Path(path)
+
+    def get_home_dir(self):
+        return pathlib.Path(pathlib.Path.home())
+
+    def list_files (self):
+        files = []
+        for i in self.path.iterdir():
+            if(i.is_file()):
+                files.append(i)
+        return files 
+
+    def list_dirs (self):
+        dirs = []
+        try:
+            for i in self.path.iterdir():
+                if(i.is_dir()):
+                    dirs.append(str(i.absolute()))
+        except PermissionError:
+            return False
+        except FileNotFoundError:
+            return False
+        return dirs
+
+    def list_all_dirs(self):
+        dirs = self.list_dirs()   
+        for dir_path in dirs:
+            print(dir_path)
+            self.set_path(dir_path)
+            self.list_all_dirs()
+
+    def find_file(self, query):
+        dirs = self.list_dirs()
+        for dir_path in dirs:
+            for f in self.path.glob(query):
+                print(f)
+            self.set_path(dir_path)
+            self.find_file(query)
+            
+
+    def run(self):
+        return self.list_dirs()
+
+
+
+
